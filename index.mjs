9ef49953100ef6c99aa4aae5a2c455ca90f58717
Bilal Javaid <sb100513@gmail.com>
2023-08-13 09:54:05 +0500

    server

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express();
+const PORT = 3000;
+
+app.get('/',(req, res)=>{
+    res.send('abhi ap home page pe hein' + new Date())
+})
+
+app.get('about',(req, res)=>{
+    res.send('abhi ap about page pe hein' + new Date())
+})
+
+app.get('contact',(req, res)=>{
+    res.send('abhi ap contact page pe hein' + new Date())
+})
+
+app.get('dashboard',(req, res)=>{
+    res.send('abhi ap dashboard page pe hein' + new Date())
+})
+
+app.listen(PORT,()=>{
+    console.log(`apka port okay chl rha ha number ${PORT} pe`)
+})
